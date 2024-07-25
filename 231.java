5e04eea595869b58830c1c1ee7f2f1f5bfafd762
Myth_"-'_N <110097015+Mathinesan@users.noreply.github.com>
2024-07-25 15:33:38 +0530

    Update 231.java

--- 231.java
+++ 231.java
@@ -1,7 +1,19 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        double sqrt = Math.sqrt(n);
-        int num = (int)sqrt;
-        return num*num == n;
+        // double sqrt = Math.sqrt(n);
+        // int num = (int)sqrt;
+        // return num*num == n;
+
+        int i=1;
+        while(i<n){
+            i=i*2;
+        }
+
+        if(i==n){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 }
