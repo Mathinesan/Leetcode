c2da99e154c0fe3a8832d01cd52e01f7197d3dbf
Myth_"-'_N <110097015+Mathinesan@users.noreply.github.com>
2024-07-26 14:28:44 +0530

    Create 137.java

---
+++ 137.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int[] freq = new int[nums.length];
+        
+        for (int i = 0; i < nums.length; i++) {
+            freq[i] = 0;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                if (nums[i] == nums[j]) {
+                    freq[i]++; 
+                }
+            }
+        }
+
+        for (int i = 0; i < freq.length; i++) {
+            if (freq[i] == 1) {
+                return nums[i]; 
+            }
+        }
+
+        return -1; 
+    }
+}
