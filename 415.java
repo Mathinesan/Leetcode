3fe59118795847b459110be40fe8396d6f8e232d
Myth_"-'_N <110097015+Mathinesan@users.noreply.github.com>
2024-07-28 11:57:12 +0530

    Update 415.java

--- 415.java
+++ 415.java
@@ -1,9 +1,11 @@
 class Solution {
     public String addStrings(String num1, String num2) {
-        int sum =0 ;
-        int n1 = Integer.parseInt(String.valueOf(num1));
-        int n2 = Integer.parseInt(String.valueOf(num2));
-        sum = n1+n2;
-        return String.valueOf(sum);
+        
+        BigInteger n1 = new BigInteger(num1);
+        BigInteger n2 = new BigInteger(num2);
+        
+        BigInteger sum = n1.add(n2);
+        
+        return sum.toString();
     }
 }
