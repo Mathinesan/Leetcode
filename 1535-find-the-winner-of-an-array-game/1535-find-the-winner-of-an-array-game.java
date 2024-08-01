60ae9e1edb5ac06e7a7b2a2ea2edbc52805a6169
Myth_"-'_N <110097015+Mathinesan@users.noreply.github.com>
2024-08-01 16:19:44 +0530

    Update 1535-find-the-winner-of-an-array-game.java

--- 1535-find-the-winner-of-an-array-game/1535-find-the-winner-of-an-array-game.java
+++ 1535-find-the-winner-of-an-array-game/1535-find-the-winner-of-an-array-game.java
@@ -1,21 +1,27 @@
 class Solution {
     public int getWinner(int[] arr, int k) {
-        int currentWinner = arr[0];
-        int winningStreak = 0;
-        
-        for (int i = 1; i < arr.length; i++) {
-            if (arr[i] > currentWinner) {
-                currentWinner = arr[i];
-                winningStreak = 1;
-            } else {
-                winningStreak++;
+        int count = 0;
+        Map<Integer, Integer> maps = new HashMap<>();
+        while(count!=2){
+            if(arr[0]>arr[1]){
+                int temp = arr[1];
+                for(int i=1;i<arr.length-1;i++){
+                    arr[i] = arr[i+1];
+                    count++;
+                }
+                arr[arr.length-1]=temp;
+                maps.put(arr[0],count);
             }
-            
-            if (winningStreak == k) {
-                return currentWinner;
+            if(arr[0]<arr[1]){
+                int temp = arr[0];
+                arr[0] = arr[1];
+                for(int i=1;i<arr.length-1;i++){
+                    arr[i] = arr[i+1];
+                }
+                arr[arr.length-1] = temp;
+                maps.put(arr[0],count++);
             }
+
         }
-        
-        return currentWinner;
     }
 }
