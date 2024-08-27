84117f12200fcb76e93d4bb7eb1bbea18d461d82
Myth_"-'_N <110097015+Mathinesan@users.noreply.github.com>
2024-08-27 23:32:10 +0530

    Time: 4 ms (52.85%), Space: 53.5 MB (5.07%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,15 +1,7 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        HashMap<Integer,Integer> hash = new HashMap<>();
-        int result = 0;
-        int maxi = 0;
-        for(int i : nums){
-            hash.put(i, hash.getOrDefault(i , 0)+1);
-            if(hash.get(i)>maxi){
-                result = i;
-                maxi = hash.get(i);
-            }
-        }
-        return result;
+        Arrays.sort(nums);
+        int n = nums.length;
+        return nums[n/2];
     }
 }
