f98ad9a363242e74e5be7b0ad960206b50ede6d4
Myth_"-'_N <110097015+Mathinesan@users.noreply.github.com>
2024-09-15 14:26:36 +0530

    Time: 18 ms (79.21%), Space: 94.7 MB (20.81%) - LeetHub

--- 0077-combinations/0077-combinations.java
+++ 0077-combinations/0077-combinations.java
@@ -1,13 +1,13 @@
 class Solution {
     public List<List<Integer>> combine(int n, int k) {
-        List<Integet> current = new ArrayList<>();
+        List<Integer> current = new ArrayList<>();
         List<List<Integer>> result = new ArrayList<>();
 
         backtrack(n,k,1,current,result);
         return result;
     }
 
-    public static void backtrack(int n,int k,int start, List<Integet> current,List<List<Integer>> result){
+    public static void backtrack(int n,int k,int start, List<Integer> current,List<List<Integer>> result){
         if(current.size()==k){
             result.add(new ArrayList<>(current));
             return;
