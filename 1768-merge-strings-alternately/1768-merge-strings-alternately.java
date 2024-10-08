4fc2d6a0e4532625d6a4755189fe6b59ecfe987c
Myth_"-'_N <110097015+Mathinesan@users.noreply.github.com>
2024-10-08 21:57:49 +0530

    Time: 8 ms (7.63%), Space: 42.9 MB (10.88%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        String res ="";
+        int i=0;
+        while(i<word1.length() || i<word2.length()){
+            if(i<word1.length()){
+                res+=word1.charAt(i);
+            }
+            if(i<word2.length()){
+                res+=word2.charAt(i);
+            }
+            i++;
+        }
+        //return res+=word2.substring(i);
+        return res;
+    }
+}
