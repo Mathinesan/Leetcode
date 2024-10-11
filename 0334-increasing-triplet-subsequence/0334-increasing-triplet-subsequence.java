bcc8ec2388831a165b425905bd2243431453268c
Myth_"-'_N <110097015+Mathinesan@users.noreply.github.com>
2024-10-11 14:27:55 +0530

    Time: 2 ms (90.7%), Space: 134 MB (37.08%) - LeetHub

---
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean increasingTriplet(int[] nums) {
+        int m1 = Integer.MAX_VALUE;
+        int m2 = Integer.MAX_VALUE;
+        for(int i : nums){
+            if(i<=m1) m1=i;
+            else if(i<=m2) m2=i;
+            else return true;
+        }
+        return false;
+    }
+}
