e692f560f8b5039bad718b078ce40965d936f48d
Myth_"-'_N <110097015+Mathinesan@users.noreply.github.com>
2024-11-04 10:03:59 +0530

    Time: 1 ms (99.61%), Space: 43.6 MB (87.11%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int compress(char[] chars) {
+        if(chars.length==0){
+            return 0;
+        }
+        StringBuilder str = new StringBuilder();
+        int count = 1;
+
+        for(int i=1;i<chars.length;i++){
+            if(chars[i]==chars[i-1]){
+                count++;
+            }
+            else{
+                str.append(chars[i-1]);
+                if(count>1){
+                    str.append(count);
+                }
+
+                count=1;
+            }
+        }
+        str.append(chars[chars.length-1]);
+        if(count>1) str.append(count);
+
+        for(int i=0;i<str.length();i++){
+            chars[i] = str.charAt(i);
+        }
+
+        return str.length();
+    }
+}
